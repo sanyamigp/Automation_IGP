a7df089bf5ef2d16aa0c48b1f216db6e144bd69d
Sanyam Arora <sanyam.arora@testingxperts.com>
2017-04-12 12:12:07 +0530

    Class changed

--- src/test/java/listeners/ExecutionStartEndListner.java
+++ src/test/java/listeners/ExecutionStartEndListner.java
@@ -42,18 +42,18 @@ public class ExecutionStartEndListner extends Utility implements IExecutionListe
 		
 		//3. Report open for view
 
-		String htmlReportFile = System.getProperty("user.dir") + "\\" + Utility.getValue("HtmlReportFullPath");
-		File f = new File(htmlReportFile);
-		if (f.exists()) {
-
-			try {
-				Process p = Runtime.getRuntime()
-						.exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe \"" + htmlReportFile
-								+ "\"");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+//		String htmlReportFile = System.getProperty("user.dir") + "\\" + Utility.getValue("HtmlReportFullPath");
+//		File f = new File(htmlReportFile);
+//		if (f.exists()) {
+//
+//			try {
+//				Process p = Runtime.getRuntime()
+//						.exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe \"" + htmlReportFile
+//								+ "\"");
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
 		//4. Generate Comparison sheet 
 		ReportFactoryDB.getComparisonReport(GlobalUtil.getCommonSettings().getProjectName());
 		
