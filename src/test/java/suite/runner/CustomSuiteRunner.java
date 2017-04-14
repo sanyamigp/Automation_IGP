4fab381ad25914c1d570a1649bbe4554ecbc1496
Sanyam Arora <sanyam.arora@testingxperts.com>
2017-04-14 20:20:08 +0530

    Files changed

--- src/test/java/suite/runner/CustomSuiteRunner.java
+++ src/test/java/suite/runner/CustomSuiteRunner.java
@@ -11,7 +11,6 @@ import org.testng.annotations.Test;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlPackage;
 import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlSuite.ParallelMode;
 import org.testng.xml.XmlTest;
 
 import utilities.ExcelDataUtil;
